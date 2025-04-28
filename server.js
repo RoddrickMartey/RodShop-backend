e195332da1c1e1b6b8f95a509444b673cb0d767d
Roddrick Martey <roddrickmartey2@gmail.com>
2025-04-28 16:21:02 +0000

    RodShop Backend init

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"; // Import the CORS package
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const VISITORURL = process.env.VISITORURL;
+
+// CORS configuration
+const corsOptions = {
+  origin: [VISITORURL], // Your frontend URL
+  credentials: true, // Allow cookies to be sent with the request
+};
+
+// Middleware
+app.use(cors(corsOptions)); // Apply CORS middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Route Handlers
+
+// Simple route for testing
+app.get("/api", (req, res) => {
+  res.send("Hello, welcome to my portfolio backend!");
+});
+
+// Set the server to listen on port 5000 (or use the environment port)
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  if (process.env.NODE_ENV === "development") {
+    console.log(`Server is running on port ${port}`);
+  }
+});
